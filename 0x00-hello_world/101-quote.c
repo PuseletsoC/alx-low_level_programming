7d63f5c620c97a15c35c7deb04853cdc335a4ddc
PuseletsoC <pusechabana@gmail.com>
2023-04-24 13:20:08 -0700

    Positive anything is better than negative nothing

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 include <unistd.h>
-
 /**
  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  *                followed by a new line, to standard error.
@@ -8,9 +7,6 @@ include <unistd.h>
  */
 int main(void)
 {
-	write(2,
-	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	      59);
-
-	return (1);
+	   write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	   return (1);
 }
