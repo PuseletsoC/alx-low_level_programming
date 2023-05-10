332ffbd1b5c2a0de99f4fc06f99f79b0ec89525c
PuseletsoC <pusechabana@gmail.com>
2023-05-10 11:22:53 -0700

    Inception Now before you bother telling me its impossible

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 int check_pal(char *s, int i, int len);
-
 int _strlen_recursion(char *s);
 
 /**
@@ -13,9 +12,7 @@ int _strlen_recursion(char *s);
 int is_palindrome(char *s)
 {
 	if (*s == 0)
-
 		return (1);
-
 	return (check_pal(s, 0, _strlen_recursion(s)));
 }
 
@@ -28,9 +25,7 @@ int is_palindrome(char *s)
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-
 		return (0);
-
 	return (1 + _strlen_recursion(s + 1));
 }
 
@@ -45,12 +40,8 @@ int _strlen_recursion(char *s)
 int check_pal(char *s, int i, int len)
 {
 	if (*(s + i) != *(s + len - 1))
-
 		return (0);
-
 	if (i >= len)
-
 		return (1);
-
 	return (check_pal(s, i + 1, len - 1));
 }
